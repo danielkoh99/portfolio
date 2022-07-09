b2edc730a18f63d1de64c240e512952a100ad4e1
danielkoh99 <kohari.daniel@gmail.com>
2022-07-10 00:04:31 +0200

    fixed layout

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -74,7 +74,7 @@ export default function About() {
               fontWeight={500}
               marginBottom={4}
             >
-              <Heading as="h4" size="md" marginBottom={2}>
+              <Heading as="h4" size="md" marginBottom={3}>
                 {text.header}
               </Heading>
               <UnorderedList>
